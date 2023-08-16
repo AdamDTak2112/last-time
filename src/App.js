747537fa8d63b9c3b029f189cd59bff9f0017b64
Adam Tak <adam.tak2112@gmail.com>
2023-08-16 10:36:42 -0600

    Add date component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Date from './components/Date';
 
 function App() {
   return (
     <div className="App">
       <h1>When Did Ein Pee Last?</h1>
+     
     </div>
   );
 }
