516df20d5bc68b9da68c87a112e7f57b141427c2
Adam Tak <adam.tak2112@gmail.com>
2023-08-17 09:52:14 -0600

    fixed handleclick logic

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import TodaysDate from './components/TodaysDate';
 import {Button} from 'react-bootstrap';
 import DisplayBox from './components/DisplayBox';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
--- src/components/DisplayBox.js
+++ src/components/DisplayBox.js
@@ -5,20 +5,23 @@ import Button from "react-bootstrap/Button";
 class DisplayBox extends React.Component{
     constructor(){
         super();
+        this.state={
+            
+        };
 
-
+        this.handleClick = this.handleClick.bind(this);
     }
 
-    handleClick = () => {
-        
-    };
+    handleClick() {
+        console.log("clicked");
+    }
 
     render(){
         return (
-            <div>
+            <div className="container">
                 <h1>When Did Ein Pee Last?</h1>
                 <TodaysDate/>
-                <Button onClick={handleClick}/>
+                <Button onClick={this.handleClick}/>
             </div>
         );
     }
