ff1572d00b84455e4f8b803937e96bca3921d5c7
Adam Tak <adam.tak2112@gmail.com>
2023-08-21 10:44:25 -0600

    add realtime database

--- src/services/firebase.config.js
+++ src/services/firebase.config.js
@@ -1,5 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getDatabase } from "firebase/database";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -10,8 +11,13 @@ const firebaseConfig = {
   projectId: "last-time-9f837",
   storageBucket: "last-time-9f837.appspot.com",
   messagingSenderId: "470136881068",
-  appId: "1:470136881068:web:9f1ae22ae741ecdaf70409"
+  appId: "1:470136881068:web:9f1ae22ae741ecdaf70409",
+  databaseURL: "https://last-time-9f837-default-rtdb.firebaseio.com/"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+
+// Initialize Realtime Database
+const database = getDatabase(app);
