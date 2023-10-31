312902e3ced009bd6254a44a8ae3093a11264d18
Adam Tak <adam.tak2112@gmail.com>
2023-10-31 08:48:20 -0600

    downsized so i can use config as modular file

--- src/services/firebase.config.js
+++ src/services/firebase.config.js
@@ -1,11 +1,5 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getDatabase } from "firebase/database";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
 // Your web app's Firebase configuration
-const firebaseConfig = {
+export const firebaseConfig = {
   apiKey: "AIzaSyDioJU2IM23PB_znhYbTa7W6inap0vDizM",
   authDomain: "last-time-9f837.firebaseapp.com",
   projectId: "last-time-9f837",
@@ -14,10 +8,3 @@ const firebaseConfig = {
   appId: "1:470136881068:web:9f1ae22ae741ecdaf70409",
   databaseURL: "https://last-time-9f837-default-rtdb.firebaseio.com/"
 };
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-
-
-// Initialize Realtime Database
-const database = getDatabase(app);
